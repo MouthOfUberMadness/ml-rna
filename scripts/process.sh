d77a8eada70eae2928c2ce71e05860e6111def83
MouthOfUberMadness <francois.calderon@gmail.com>
2021-11-22 09:16:58 +0100

    Update

--- scripts/process.sh
+++ scripts/process.sh
@@ -2,7 +2,7 @@
 sed -r 's/, complete genome//' $1.fasta >$1_1
 sed '/^[^>]/s/T/U/g' $1_1 >$1_processed.fasta
 # sed -r 's/, complete genome//' $1_raw.fasta >$1.fasta
-mafft $1_processed.fasta > $1_raw.aln
+mafft --maxiterate 1000 --thread 12 $1_processed.fasta > $1_raw.aln
 sed '/^[^>]/s/-/n/g' $1_raw.aln >$1.aln
 rm $1_1
 rm $1_processed.fasta
--- src/analysis.cpp
+++ src/analysis.cpp
@@ -4,12 +4,58 @@
 
 #include <Bpp/Seq/Alphabet/RNA.h>
 #include <Bpp/Seq/Io/Fasta.h>
+#include <Bpp/Seq/SequencePositionIterators.h>
+#include <Bpp/Seq/SequenceWalker.h>
 
 #include <Bpp/Phyl/Tree/PhyloTreeTools.h>
 
 #include <Bpp/Phyl/Io/Newick.h>
 
 #include "analysis.h"
+
+void processSequence(const bpp::AlignedSequenceContainer &alignedSequences,
+                     size_t seqId) {
+  //   auto walker = bpp::SequenceWalker(sequence);
+
+  auto alpha = std::make_shared<bpp::RNA>();
+  std::string codon;
+  size_t cursor = 0;
+  size_t cursorCodon = 0;
+  bool isTheEnd = false;
+
+  size_t numSites = alignedSequences.getNumberOfSites();
+  size_t numBins = 20;
+
+  std::vector<size_t> histogram(numBins);
+  std::string aCodon = "CGG";
+//   std::string aCodon = "UCC";
+  while (cursor < numSites) {
+
+    bpp::Site site = alignedSequences.getSite(cursor);
+    if (seqId < site.size()) {
+      auto nucleotide = alpha.get()->intToChar(site[seqId]);
+      //   if (cursor < 100)
+      //     std::cout << "cursor " << cursor << " " << nucleotide << std::endl;
+      if (nucleotide != "N")
+        codon.append(nucleotide);
+    }
+
+    if (codon.size() == 3) {
+      if (codon == aCodon) {
+        size_t bin = std::floor(((double)cursor / (double)numSites) * numBins);
+        histogram[bin]++;
+      }
+
+      codon.clear();
+    }
+
+    cursor++;
+  }
+
+  for (size_t bin : histogram) {
+    std::cout << bin << std::endl;
+  }
+}
 
 void analysePhylogeneticTree(const std::string &filename) {
   size_t lastindex = filename.find_last_of(".");
@@ -23,4 +69,6 @@ void analysePhylogeneticTree(const std::string &filename) {
   auto alpha = std::make_shared<bpp::RNA>();
   auto sites = std::shared_ptr<bpp::AlignedSequenceContainer>(
       Fst.readAlignment(sequencesName, alpha.get()));
+
+  processSequence(*sites, 0);
 }
