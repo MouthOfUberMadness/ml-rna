d85a6d689a66a340d8304f363be705484338579b
MouthOfUberMadness <francois.calderon@gmail.com>
2021-11-25 22:52:26 +0100

    Update

--- src/fcsPatterns.h
+++ src/fcsPatterns.h
@@ -19,121 +19,170 @@ enum class FCSPatterns {
   RRXRR,
 };
 
-std::unordered_set<std::string>
-getFcsPatterns(FCSPatterns fcsPattern, bool almost, bool debug = false) {
+std::vector<std::string> replaceX(const std::vector<std::string> &input,
+                                  size_t sizePattern, bool debug = false) {
+  std::vector<std::string> output = input;
+  for (size_t i = 0; i < sizePattern; i++) {
+    std::vector<std::string> temp;
+    for (auto pattern : output) {
+      if (pattern[i] == 'X') {
+        pattern[i] = 'U';
+        temp.push_back(pattern);
+        pattern[i] = 'C';
+        temp.push_back(pattern);
+        pattern[i] = 'A';
+        temp.push_back(pattern);
+        pattern[i] = 'G';
+        temp.push_back(pattern);
+      } else {
+        temp.push_back(pattern);
+      }
+    }
+    output = temp;
+    if (debug) {
+      std::cout << "updated pattern " << std::endl;
+      for (auto pattern : output) {
+        std::cout << pattern << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+  return output;
+}
 
+std::unordered_set<std::string> getRXXRPatterns(bool almost,
+                                                bool debug = false) {
   std::vector<std::string> listR = {"CGU", "CGC", "CGA", "CGG", "AGA", "AGG"};
-  std::vector<std::string> patterns;
-  std::vector<std::string> almostPatterns;
+  std::vector<std::string> patterns = listR;
   std::vector<std::string> fcsPatterns;
+
+  for (size_t i = 0; i < patterns.size(); i++)
+    for (size_t j = 0; j < patterns.size(); j++) {
+      if (almost) {
+        for (size_t l = 0; l < 3; l++) {
+          auto almostPattern =
+              patterns[i] + std::string("XXXXXX") + patterns[j];
+          almostPattern[l] = 'X';
+          fcsPatterns.push_back(almostPattern);
+        }
+        for (size_t l = 0; l < 3; l++) {
+          auto almostPattern =
+              patterns[i] + std::string("XXXXXX") + patterns[j];
+          almostPattern[9 + l] = 'X';
+          fcsPatterns.push_back(almostPattern);
+        }
+      } else {
+        fcsPatterns.push_back(patterns[i] + std::string("XXXXXX") +
+                              patterns[j]);
+      }
+    }
+
+  if (debug) {
+    for (auto pattern : fcsPatterns) {
+      std::cout << pattern << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  fcsPatterns = replaceX(fcsPatterns, 12, debug);
+
+  if (debug) {
+    for (auto pattern : fcsPatterns) {
+      std::cout << pattern << " ";
+    }
+    std::cout << std::endl;
+  }
+
   std::unordered_set<std::string> fcsPatternsSet;
-
-  patterns = listR;
-
-  if (fcsPattern == FCSPatterns::RXXR) {
-    for (size_t i = 0; i < patterns.size(); i++)
-      for (size_t j = 0; j < patterns.size(); j++) {
-        if (almost) {
-          for (size_t l = 0; l < 3; l++) {
-            auto almostPattern =
-                patterns[i] + std::string("XXXXXX") + patterns[j];
-            almostPattern[l] = 'X';
-            fcsPatterns.push_back(almostPattern);
-          }
-          for (size_t l = 0; l < 3; l++) {
-            auto almostPattern =
-                patterns[i] + std::string("XXXXXX") + patterns[j];
-            almostPattern[9 + l] = 'X';
-            fcsPatterns.push_back(almostPattern);
-          }
-        } else {
-          fcsPatterns.push_back(patterns[i] + std::string("XXXXXX") +
-                                patterns[j]);
-        }
-      }
-  } else if (fcsPattern == FCSPatterns::RXRROrRRXR) {
-    for (size_t i = 0; i < patterns.size(); i++)
-      for (size_t j = 0; j < patterns.size(); j++) {
-        for (size_t k = 0; k < patterns.size(); k++) {
-          auto patternLeft =
-              patterns[i] + std::string("XXX") + patterns[j] + patterns[k];
-          auto patternRight =
-              patterns[i] + patterns[j] + std::string("XXX") + patterns[k];
-
-          if (almost) {
-            for (size_t l = 0; l < 3; l++) {
-              auto almostPatternLeft = patternLeft;
-              auto almostPatternRight = patternRight;
-              almostPatternLeft[l] = 'X';
-              almostPatternRight[9 + l] = 'X';
-              fcsPatterns.push_back(almostPatternLeft);
-              fcsPatterns.push_back(almostPatternRight);
-            }
-            for (size_t l = 0; l < 6; l++) {
-              auto almostPatternLeft = patternLeft;
-              auto almostPatternRight = patternRight;
-              almostPatternLeft[6 + l] = 'X';
-              almostPatternRight[l] = 'X';
-              fcsPatterns.push_back(almostPatternLeft);
-              fcsPatterns.push_back(almostPatternRight);
-            }
-          } else {
-            fcsPatterns.push_back(patternLeft);
-            fcsPatterns.push_back(patternRight);
-          }
-        }
-      }
-  } else if (fcsPattern == FCSPatterns::RRXRR) {
-  }
-
-  if (debug) {
-    for (auto pattern : fcsPatterns) {
-      std::cout << pattern << " ";
-    }
-    std::cout << std::endl;
-  }
-
-  if (fcsPattern == FCSPatterns::RXXR ||
-      fcsPattern == FCSPatterns::RXRROrRRXR) {
-    for (size_t i = 0; i < 12; i++) {
-      std::vector<std::string> tempFcsPatterns;
-      for (auto pattern : fcsPatterns) {
-        if (pattern[i] == 'X') {
-          pattern[i] = 'U';
-          tempFcsPatterns.push_back(pattern);
-          pattern[i] = 'C';
-          tempFcsPatterns.push_back(pattern);
-          pattern[i] = 'A';
-          tempFcsPatterns.push_back(pattern);
-          pattern[i] = 'G';
-          tempFcsPatterns.push_back(pattern);
-        } else {
-          tempFcsPatterns.push_back(pattern);
-        }
-      }
-      fcsPatterns = tempFcsPatterns;
-      if (debug) {
-        std::cout << "updated pattern " << std::endl;
-        for (auto pattern : fcsPatterns) {
-          std::cout << pattern << " ";
-        }
-        std::cout << std::endl;
-      }
-    }
-  } else if (fcsPattern == FCSPatterns::RRXRR) {
-  }
-
-  if (debug) {
-    std::cout << "final pattern " << std::endl;
-    for (auto pattern : fcsPatterns) {
-      std::cout << pattern << " ";
-    }
-    std::cout << std::endl;
-  }
-
   for (auto pattern : fcsPatterns) {
     fcsPatternsSet.insert(pattern);
   }
-
   return fcsPatternsSet;
+}
+
+std::unordered_set<std::string> getRRXROrRXRRFcsPatterns(bool almost,
+                                                         bool debug = false) {
+  std::vector<std::string> listR = {"CGU", "CGC", "CGA", "CGG", "AGA", "AGG"};
+  std::vector<std::string> patterns = listR;
+  std::vector<std::string> fcsPatterns;
+
+  for (size_t i = 0; i < patterns.size(); i++)
+    for (size_t j = 0; j < patterns.size(); j++) {
+      for (size_t k = 0; k < patterns.size(); k++) {
+        auto patternLeft =
+            patterns[i] + std::string("XXX") + patterns[j] + patterns[k];
+        auto patternRight =
+            patterns[i] + patterns[j] + std::string("XXX") + patterns[k];
+
+        if (almost) {
+          for (size_t l = 0; l < 3; l++) {
+            auto almostPatternLeft = patternLeft;
+            auto almostPatternRight = patternRight;
+            almostPatternLeft[l] = 'X';
+            almostPatternRight[9 + l] = 'X';
+            fcsPatterns.push_back(almostPatternLeft);
+            fcsPatterns.push_back(almostPatternRight);
+          }
+          for (size_t l = 0; l < 6; l++) {
+            auto almostPatternLeft = patternLeft;
+            auto almostPatternRight = patternRight;
+            almostPatternLeft[6 + l] = 'X';
+            almostPatternRight[l] = 'X';
+            fcsPatterns.push_back(almostPatternLeft);
+            fcsPatterns.push_back(almostPatternRight);
+          }
+        } else {
+          fcsPatterns.push_back(patternLeft);
+          fcsPatterns.push_back(patternRight);
+        }
+      }
+    }
+
+  if (debug) {
+    for (auto pattern : fcsPatterns) {
+      std::cout << pattern << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  fcsPatterns = replaceX(fcsPatterns, 12, debug);
+
+  if (debug) {
+    for (auto pattern : fcsPatterns) {
+      std::cout << pattern << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  std::unordered_set<std::string> fcsPatternsSet;
+  for (auto pattern : fcsPatterns) {
+    fcsPatternsSet.insert(pattern);
+  }
+  return fcsPatternsSet;
+}
+
+std::unordered_set<std::string> getFcsPatterns(FCSPatterns fcsPattern,
+                                               bool almostExclusive,
+                                               bool debug = false) {
+  std::unordered_set<std::string> fcsPatternsSet;
+  std::unordered_set<std::string> almostFcsPatternsSet;
+
+  if (fcsPattern == FCSPatterns::RXXR) {
+    fcsPatternsSet = getRXXRPatterns(false, debug);
+    if (almostExclusive)
+      almostFcsPatternsSet = getRXXRPatterns(true, debug);
+  } else if (fcsPattern == FCSPatterns::RXRROrRRXR) {
+    fcsPatternsSet = getRRXROrRXRRFcsPatterns(false, debug);
+    if (almostExclusive)
+      almostFcsPatternsSet = getRRXROrRXRRFcsPatterns(true, debug);
+  }
+
+  if (almostExclusive) {
+    for (auto pattern : fcsPatternsSet) {
+      almostFcsPatternsSet.erase(pattern);
+    }
+    return almostFcsPatternsSet;
+  } else {
+    return fcsPatternsSet;
+  }
 }
